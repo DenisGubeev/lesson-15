d51a4d3eb892e01e34a98b00cfcdf7072ea7560d
Denis <35633718+DenisGubeev@users.noreply.github.com>
2018-03-30 22:14:19 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,5 @@
 <?php
 
-	$host = 'localhost';
-	$dbname = 'CSV_DB';
-	$dbuser = 'root';
-	$dbpassword = '';
-
-
-try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $dbuser, $dbpassword);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    
-     $sql = "CREATE TABLE `students` (
-	`id` int(11) NOT NULL AUTO_INCREMENT,
-	`name` varchar(50) NOT NULL,
-	`estimation` float NOT NULL,
-	`budget` tinyint(4) NOT NULL DEFAULT '0',
-	PRIMARY KEY (`id`)
-	) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
- 
-    $conn->exec($sql);
-    echo "Table students created successfully";
-    }
-catch(PDOException $e)
-    {
-    echo $sql . "<br>" . $e->getMessage();
-    }
-$conn = null;
-
 $sql = "SHOW TABLES FROM $dbname";
 $statement = $pdo->prepare($sql);
 $statement->execute();
